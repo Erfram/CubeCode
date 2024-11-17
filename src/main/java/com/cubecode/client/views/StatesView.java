080937957f7dc27a9851b486fcedeeac23f616cb
InkyDemon <inkydmitrii@gmail.com>
2024-11-17 17:05:11 +0500

    update StatesView

--- src/main/java/com/cubecode/client/views/StatesView.java
+++ src/main/java/com/cubecode/client/views/StatesView.java
@@ -1,18 +1,14 @@
 package com.cubecode.client.views;
 
-import com.cubecode.client.imgui.CubeImGui;
 import com.cubecode.client.imgui.basic.View;
 import com.cubecode.client.imgui.components.Window;
-import com.cubecode.utils.Icons;
 import imgui.ImGui;
-import imgui.ImVec2;
-import imgui.flag.ImGuiCond;
-import imgui.flag.ImGuiSelectableFlags;
-import imgui.flag.ImGuiWindowFlags;
-import imgui.type.ImBoolean;
-import net.minecraft.nbt.NbtCompound;
+import imgui.flag.*;
+import imgui.type.*;
+import net.minecraft.nbt.*;
 
 import java.util.List;
+import java.util.Set;
 
 public class StatesView extends View {
     NbtCompound nbt;
@@ -45,18 +41,13 @@ public class StatesView extends View {
         Window.create()
             .title(getName())
             .callback(() -> {
-                ImGui.setCursorPosX((ImGui.getWindowWidth() - ImGui.calcTextSize(this.currentState).x) / 2);
-                ImGui.textColored(255,207,72, 255, this.currentState.replaceAll("\\^", ""));
-                if (ImGui.imageButton(Icons.PLUS.getGlId(), 16, 16)) {
-                    ImVec2 center = ImGui.getMainViewport().getCenter();
-                    ImGui.setNextWindowPos(center.x, center.y, ImGuiCond.Always, 0.5f, 0.5f);
-                    ImGui.openPopup("lox");
-                }
+                ImGui.pushStyleVar(ImGuiStyleVar.FrameBorderSize, 1.0f);
+                ImGui.pushStyleVar(ImGuiStyleVar.FrameRounding, 0.0f);
 
-                if (ImGui.beginPopupModal("lox", new ImBoolean(true), ImGuiWindowFlags.NoDecoration | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.AlwaysAutoResize)) {
-                    CubeImGui.selectable("Server", this.currentState.equals("Server^"), Icons.SERVER, ImGuiSelectableFlags.None, () -> {
+                if (ImGui.beginCombo("##target", this.currentState.replace("^", ""))) {
+                    if (ImGui.selectable("Server", this.currentState.equals("Server^"))) {
                         this.currentState = "Server^";
-                    });
+                    }
 
                     this.playerNames.forEach(playerName -> {
                         if (ImGui.selectable(playerName, this.currentState.equals(playerName))) {
@@ -64,11 +55,93 @@ public class StatesView extends View {
                         }
                     });
 
-                    ImGui.endPopup();
+                    ImGui.endCombo();
                 }
 
+                ImGui.popStyleVar(2);
+                ImGui.popItemWidth();
                 ImGui.separator();
+
+                NbtCompound statesCompound = this.nbt.getCompound(this.currentState);
+                Set.of(statesCompound.getKeys().toArray(String[]::new)).forEach(stateKey -> {
+                    NbtElement stateValue = statesCompound.get(stateKey);
+
+                    this.putVariable(stateKey, new ImString(stateKey, 9999));
+                    String valueKey = this.getValueKey(stateKey);
+                    ImString newStateKey = this.getVariable(stateKey);
+                    boolean contains = !stateKey.equals(newStateKey.get()) && statesCompound.getKeys().contains(newStateKey.get());
+
+                    ImGui.pushItemWidth(150);
+                    ImGui.pushStyleVar(ImGuiStyleVar.FrameBorderSize, 1.0f);
+                    ImGui.pushStyleVar(ImGuiStyleVar.FrameRounding, 0.0f);
+
+                    if (contains) {
+                        ImGui.pushStyleColor(ImGuiCol.Text, 255, 0, 0, 255);
+                    }
+
+                    if (!ImGui.inputText("##" + stateKey, newStateKey) && ImGui.isItemDeactivatedAfterEdit()) {
+                        if (!statesCompound.getKeys().contains(newStateKey.get())) {
+                            this.removeVariable(stateKey);
+                            this.removeVariable(this.getValueKey(stateKey));
+                            statesCompound.remove(stateKey);
+                            statesCompound.put(newStateKey.get(), stateValue);
+                            valueKey = this.getValueKey(newStateKey.get());
+                        }
+                        else if (!stateKey.equals(newStateKey.get())) {
+                            newStateKey.set(stateKey);
+                        }
+                    }
+
+                    if (contains) {
+                        ImGui.popStyleColor();
+                    }
+
+                    ImGui.popStyleVar(2);
+                    ImGui.popItemWidth();
+                    ImGui.sameLine();
+
+                    ImGui.pushStyleVar(ImGuiStyleVar.FrameBorderSize, 1.0f);
+                    ImGui.pushStyleVar(ImGuiStyleVar.FrameRounding, 0.0f);
+                    if (stateValue.getType() == NbtElement.INT_TYPE) {
+                        this.putVariable(valueKey, new ImInt(((NbtInt) stateValue).intValue()));
+                        ImInt value = this.getVariable(valueKey);
+
+                        if (!ImGui.dragScalar("##" + valueKey, ImGuiDataType.S32, value, 1f) && ImGui.isItemDeactivatedAfterEdit()) {
+                            statesCompound.putInt(newStateKey.get(), value.get());
+                        }
+                    }
+                    else if (stateValue.getType() == NbtElement.DOUBLE_TYPE) {
+                        this.putVariable(valueKey, new ImDouble(((NbtDouble) stateValue).doubleValue()));
+                        ImDouble value = this.getVariable(valueKey);
+
+                        if (!ImGui.dragScalar("##" + valueKey, ImGuiDataType.Double, value, 1f) && ImGui.isItemDeactivatedAfterEdit()) {
+                            statesCompound.putDouble(newStateKey.get(), value.doubleValue());
+                        }
+                    }
+                    else if (stateValue.getType() == NbtElement.FLOAT_TYPE) {
+                        this.putVariable(valueKey, new ImFloat(((NbtFloat) stateValue).floatValue()));
+                        ImFloat value = this.getVariable(valueKey);
+
+                        if (!ImGui.dragScalar("##" + valueKey, ImGuiDataType.Float, value, 1f) && ImGui.isItemDeactivatedAfterEdit()) {
+                            statesCompound.putFloat(newStateKey.get(), value.floatValue());
+                        }
+                    }
+                    else if (stateValue.getType() == NbtElement.STRING_TYPE) {
+                        this.putVariable(valueKey, new ImString(stateValue.asString(), 9999));
+                        ImString value = this.getVariable(valueKey);
+
+                        if (!ImGui.inputText("##" + valueKey, value) && ImGui.isItemDeactivatedAfterEdit()) {
+                            statesCompound.putString(newStateKey.get(), value.get());
+                        }
+                    }
+
+                    ImGui.popStyleVar(2);
+                });
             })
             .render(this);
+    }
+
+    private String getValueKey(String stateKey) {
+        return stateKey + "_value";
     }
 }
