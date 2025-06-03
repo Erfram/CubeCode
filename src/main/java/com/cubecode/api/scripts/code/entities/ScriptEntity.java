301fec9834647cb03a1f4498f1737bb3383ed2a8
llama <jenya-vev@list.ru>
2025-06-04 01:58:54 +0300

    API: - ScriptEntity - ScriptPlayer

---
+++ src/main/java/com/cubecode/api/scripts/code/entities/ScriptEntity.java
@@ -0,0 +1,23 @@
+package com.cubecode.api.scripts.code.entities;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public class ScriptEntity<T extends Entity> {
+    protected T entity;
+
+    public static ScriptEntity create(Entity entity) {
+        if (entity instanceof PlayerEntity) {
+            return new ScriptPlayer((ServerPlayerEntity) entity);
+        } else if (entity != null) {
+            return new ScriptEntity<Entity>(entity);
+        }
+
+        return null;
+    }
+
+    protected ScriptEntity(T entity) {
+        this.entity = entity;
+    }
+}
---
+++ src/main/java/com/cubecode/api/scripts/code/entities/ScriptPlayer.java
@@ -0,0 +1,16 @@
+package com.cubecode.api.scripts.code.entities;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public class ScriptPlayer extends ScriptEntity<ServerPlayerEntity> {
+    public ScriptPlayer(ServerPlayerEntity entity) {
+        super(entity);
+    }
+
+    /**
+     * Returns the Minecraft player entity associated with this script player
+     */
+    public ServerPlayerEntity getMinecraftPlayer() {
+        return this.entity;
+    }
+}
