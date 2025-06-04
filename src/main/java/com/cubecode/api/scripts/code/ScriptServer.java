72426a458474f2c59b05d4800d8a5e6a494017c1
TaBJleTKa <asdfgf4esvcr@gmail.com>
2025-06-04 16:56:35 +0300

    add new methods in ScriptServer

--- src/main/java/com/cubecode/api/scripts/code/ScriptServer.java
+++ src/main/java/com/cubecode/api/scripts/code/ScriptServer.java
@@ -2,6 +2,8 @@ package com.cubecode.api.scripts.code;
 
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.text.Text;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.GameMode;
 
 import java.util.ArrayList;
 
@@ -19,14 +21,15 @@ public class ScriptServer {
         return this.server;
     }
 
-    public int getMaxPlayerCount() {
-        return this.server.getMaxPlayerCount();
-    }
+
 
     public String[] getPlayerNames() {
         return this.server.getPlayerNames();
     }
 
+    public boolean isWhitelistEnabled() {
+        return this.server.isEnforceWhitelist();
+    }
     public boolean isSingleplayer() {
         return this.server.isSingleplayer();
     }
@@ -39,16 +42,80 @@ public class ScriptServer {
         return this.server.isHardcore();
     }
 
-
     public boolean isOnlineMode() {
         return this.server.isOnlineMode();
+    }
+
+    public boolean isNetherEnabled() {
+        return this.server.isNetherAllowed();
+    }
+
+    public boolean isPvpEnabled() {
+        return this.server.isPvpEnabled();
+    }
+
+    public boolean isMonsterSpawningEnabled() {
+        return this.server.isMonsterSpawningEnabled();
+    }
+
+    public boolean isFlightEnabled() {
+        return this.server.isFlightEnabled();
+    }
+
+    public int getPlayerIdleTimeout() {
+        return this.server.getPlayerIdleTimeout();
     }
 
     public int getTicks() {
         return this.server.getTicks();
     }
 
+    public int getMaxPlayerCount() {
+        return this.server.getMaxPlayerCount();
+    }
+
+    public int getPlayerCount() {
+        return this.server.getCurrentPlayerCount();
+    }
+
     public void send(String message, boolean overlay) {
         this.server.getPlayerManager().broadcast(Text.of(message), overlay);
     }
+
+    public void setDifficultyLocked(boolean locked) {
+        this.server.setDifficultyLocked(locked);
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.server.setDifficulty(Difficulty.byId(difficulty), false);
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.server.setDifficulty(Difficulty.byName(difficulty), false);
+    }
+
+    public void setDefaultGameMode(int gamemode) {
+        this.server.setDefaultGameMode(GameMode.byId(gamemode));
+    }
+
+    public void setDefaultGameMode(String gamemode) {
+        this.server.setDefaultGameMode(GameMode.byName(gamemode));
+    }
+
+    public void setWhitelist(boolean isEnabled) {
+        this.server.setEnforceWhitelist(isEnabled);
+    }
+
+    public void setFlightEnabled(boolean isEnabled) {
+        this.server.setFlightEnabled(isEnabled);
+    }
+
+    public void setPvpEnabled(boolean isEnabled) {
+        this.server.setPvpEnabled(isEnabled);
+    }
+
+    public void setPlayerIdleTimeout(int timeout) {
+        this.server.setPlayerIdleTimeout(timeout);
+    }
+
 }
